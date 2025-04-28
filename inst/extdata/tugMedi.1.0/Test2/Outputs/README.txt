7fc31bc679ad9d81361035970698c2c740c34cfa
tugHall <nagornov.yuri@gmail.com>
2025-04-29 01:06:28 +0900

    Tests update corresponding to tigMedi 1.0.17

---
+++ inst/extdata/tugMedi.1.0/Test2/Outputs/README.txt
@@ -0,0 +1,9 @@
+
+
+
+See: 
+https://figshare.com/s/ce76be8678d2c4d10d62
+Test2.Outputs.zip
+
+
+
---
+++ inst/extdata/tugMedi.1.0/Test2/test2.1_preABC.R
@@ -0,0 +1,56 @@
+#!/usr/bin/Rscript --slave --vanilla
+
+
+#####################################################
+##### For development with source() #################
+if (0) {
+
+    # Debugging mode
+    files <- Sys.glob( 'R/*.R' )
+    files <- files[ ! grepl('R/test|R/_', files, perl=T) ]
+    for (file in files) {
+       print( paste0( "For development, loading: ", file ) )
+       source(file)
+    } 
+    
+} else {
+    
+    # User mode
+    library(tugMedi)
+}
+#####################################################
+#flog.threshold( 'DEBUG' )
+#flog.debug( 'flog level in main: DEBUG' )
+#####################################################
+print("Generators start")
+
+
+write_prm(       input = './Input/ForGenerators/parameters.parameters.txt')
+
+#write_CF(        input = './Input/ForGenerators/CF.parameters.txt')
+
+write_weights(   input = './Input/ForGenerators/hallmark_weights.parameters.txt' )
+
+write_cloneinit( input  = './Input/ForGenerators/cloneinit.parameters.txt' )
+
+#write_Rint(      input = './Input/ForGenerators/Rint.parameters.txt' )
+
+write_EF.Rint(   input = './Input/ForGenerators/EF.Rint.parameters.txt' )
+
+# Only this depends on write_prm() result
+write_EF.Rother( input = './Input/ForGenerators/EF.Rother.parameters.txt' )
+
+
+##############################################################
+print("Simulation starts")
+
+system.time(
+start_simulation( input_files = 'Input/DATA/FILES.txt', 
+                  saveTo      = 'Output/Results.sim.01.RDS', 
+                  seed        = NA
+                 )
+)
+
+write_VAF( input = './Input/ForPosts/VAF.parameters.txt' )
+
+
